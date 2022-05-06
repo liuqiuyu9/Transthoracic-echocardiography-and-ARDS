e17eebd2675a341128916d9ecf586c89986bd88d
liuqiuyu9 <104917208+liuqiuyu9@users.noreply.github.com>
2022-05-06 22:39:47 +0800

    Create laboratory_indicators.sql

---
+++ laboratory_indicators.sql
@@ -0,0 +1,67 @@
+##Hemoglobin
+select * from mimiciii.d_labitems where label like '%hemoglobin%'
+select * from mimiciii.d_items where itemid=50811
+select * from mimiciii.d_items where itemid=51222
+
+##Platelet
+select * from mimiciii.d_labitems where label like '%Platelet%'
+select * from mimiciii.d_items where itemid=51264
+
+##Sodium
+select * from mimiciii.d_labitems where label like '%Sodium%'
+select * from mimiciii.d_items where itemid=50824
+select * from mimiciii.d_items where itemid=50983
+
+##Potassium
+select * from mimiciii.d_labitems where label like '%Potassium%'
+select * from mimiciii.d_items where itemid=50822
+select * from mimiciii.d_items where itemid=50971
+
+##Bicarbonate
+select * from mimiciii.d_labitems where label like '%icarbonate%'
+select * from mimiciii.d_items where itemid=50803
+select * from mimiciii.d_items where itemid=50882
+
+##Chloride
+select * from mimiciii.d_labitems where label like '%hloride%'
+select * from mimiciii.d_items where itemid=50806
+select * from mimiciii.d_items where itemid=50902
+
+##NTpro-BNP
+select * from mimiciii.d_labitems where label like '%NTproBNP%'
+select * from mimiciii.d_items where itemid=50963
+
+##Lactate
+select * from mimiciii.d_items where itemid=5081
+
+##Creatine Kinase
+select * from mimiciii.d_labitems where label like '%CreatineKinase%'
+select * from mimiciii.d_items where itemid=50910
+
+##pH
+select * from mimiciii.d_items where itemid=50820
+
+##PO2
+select * from mimiciii.d_items where itemid=50821
+
+##PCO2
+select * from mimiciii.d_items where itemid=50818
+
+##BUN
+select * from mimiciii.d_labitems where label like '%Urea Nitrogen%'
+select * from mimiciii.d_items where itemid=51006
+
+##WBC
+select * from mimiciii.d_labitems where label like '%WBC%'
+
+##creatinine
+select * from mimiciii.d_labitems where label like '%Creatinine%'
+select * from mimiciii.d_items where itemid=50912
+
+##Troponin
+
+
+
+
+
+
