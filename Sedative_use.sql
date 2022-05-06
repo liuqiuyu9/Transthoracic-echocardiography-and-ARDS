e228a6055d03a7ea61d8d6bac977d1f7bc979b16
liuqiuyu9 <104917208+liuqiuyu9@users.noreply.github.com>
2022-05-06 14:57:11 +0800

    Create Sedative_use.sql

---
+++ Sedative_use.sql
@@ -0,0 +1,5 @@
+select * from mimiciii.prescriptions where drug like '%idazolam%'
+select * from mimiciii.prescriptions where drug like '%iazep%' 
+select * from mimiciii.prescriptions where drug like '%lprazo%' 
+select * from mimiciii.prescriptions where drug like '%exmedet%' 
+select * from mimiciii.prescriptions where drug like '%ropofol%'
